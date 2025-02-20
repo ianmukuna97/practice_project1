f7a5a92670481b18fb9860059d89171768e2ffc4
ianmukuna97 <ianmukuna97@gmail.com>
2025-02-20 17:14:59 +0300

    Python practice updated

--- practice_9.py
+++ practice_9.py
@@ -11,4 +11,4 @@ scores= [50, 45, 60, 70, 65, 37, 42, 56, 80, 75, 82]
 # for loop
 for score in scores:
     if score >= 50 and score <=70 and score % 2 == 0: # Values btwn 50 and 70 which are divisible by 2-- % -- modulus
-      print(score)
+      print(score)                                    # modulus % brings a remainder
