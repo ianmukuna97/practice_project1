b5b7ac48f0a3e7aaeee26c35431c232993555a07
ianmukuna97 <ianmukuna97@gmail.com>
2025-03-07 02:35:40 +0300

    Python practice updated oop

---
+++ practice_15.py
@@ -0,0 +1,35 @@
+# dates
+# inheritance
+# error handling
+
+class Employee:
+    def __init__(self, name, id_no , dob, gender):
+        self.name = name
+        self.id_number = id_no
+        self.dob = dob
+        self.gender = gender
+
+    def show_details(self):
+        print(f"Name: {self.name}\nID: {self.id_number}\nDOB: {self.dob}\nGender: {self.gender}")
+
+class PermanentEmployee(Employee):
+    def __init__(self, name, id_no, dob, gender, salary):
+        super().__init__(name, id_no, dob, gender)
+        self.salary = salary
+
+    def print_salary(self):
+        print(f"Salary is ${self.salary}")
+        
+class TemporaryEmployee(Employee):
+    def __init__(self, name, id_no, dob, gender, hourly_pay, end_date):
+        super().__init__(name, id_no, dob, gender)
+        self.hourly_pay = hourly_pay
+        self.end_date = end_date
+
+    def print_hourly_pay(self):
+        print(f"Hours pay is : ${self.hourly_pay}")
+
+    def print_end_date(self):
+        print(f"End date is: ${self.end_date}")
+
+p1 = PermanentEmployee("George James", "22334455", "7-05")
